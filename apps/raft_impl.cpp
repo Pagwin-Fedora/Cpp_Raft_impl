febbfae2579fab9d7398bb0d6d6f0696f9510808
Pagwin <git@pagwin.xyz>
2024-03-24 01:27:26 -0400

    working out early kinks due to lack of cmake xp

--- apps/raft_impl.cpp
+++ apps/raft_impl.cpp
@@ -1,6 +1,9 @@
 #include <vector>
 #include <string>
+#include <fmt/core.h>
+
 int main(int argc, char *argv[]){
     std::vector<std::string> args(argv+1, argv+argc);
+    fmt::println("Hello {}", args[0]);
     return 0;
 }
