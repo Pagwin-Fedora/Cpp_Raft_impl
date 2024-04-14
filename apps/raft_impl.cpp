bff10c78f00445cb7999a2e2c08dff5194afa4df
Pagwin <git@pagwin.xyz>
2024-04-14 18:43:22 -0400

    cmake file cleanup and c++20 test in app

--- apps/raft_impl.cpp
+++ apps/raft_impl.cpp
@@ -1,9 +1,22 @@
+#include <iostream>
 #include <vector>
 #include <string>
+#include <ranges>
 #include <fmt/core.h>
 
 int main(int argc, char *argv[]){
     std::vector<std::string> args(argv+1, argv+argc);
-    fmt::println("Hello {}", args[0]);
+
+    //simple test to make sure C++20, this should read from stdin and print out the lines it reads
+    auto lines = std::ranges::subrange(std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>())
+        | std::views::lazy_split('\n')
+        | std::views::transform([](auto view){
+                std::string s;
+                for(char c:view) s.push_back(c);
+                return s;
+                });
+
+    for(auto line:lines) fmt::println("{}",line);
+
     return 0;
 }
