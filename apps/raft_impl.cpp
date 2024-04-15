1b018bdaae9f1cacb1ea4f6993bb7e6ba559bf04
Pagwin <git@pagwin.xyz>
2024-04-15 11:22:25 -0400

    goodbye fmtlib

--- apps/raft_impl.cpp
+++ apps/raft_impl.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <ranges>
-#include <fmt/core.h>
 
 int main(int argc, char *argv[]){
     std::vector<std::string> args(argv+1, argv+argc);
     
-    for(auto& arg:args) fmt::println("{}",arg);
+    for(auto& arg:args) std::cout << arg << std::endl;
 
     return 0;
 }
