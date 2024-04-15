61d33b7e2db344603df3ef13b07196414649597d
Pagwin <git@pagwin.xyz>
2024-04-14 21:27:30 -0400

    okay cpp 17 not 20

--- apps/raft_impl.cpp
+++ apps/raft_impl.cpp
@@ -6,17 +6,8 @@
 
 int main(int argc, char *argv[]){
     std::vector<std::string> args(argv+1, argv+argc);
-
-    //simple test to make sure C++20, this should read from stdin and print out the lines it reads
-    auto lines = std::ranges::subrange(std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>())
-        | std::views::lazy_split('\n')
-        | std::views::transform([](auto view){
-                std::string s;
-                for(char c:view) s.push_back(c);
-                return s;
-                });
-
-    for(auto line:lines) fmt::println("{}",line);
+    
+    for(auto& arg:args) fmt::println("{}",arg);
 
     return 0;
 }
